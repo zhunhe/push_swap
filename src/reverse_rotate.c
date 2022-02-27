173c3947d65e42fb3bbfa9d41b1cb99528f96362
zhunhe <xuzhunhe@gmail.com>
2022-02-27 20:26:21 +0900

    fix: Change logic of rotate, reverse rotate
    - Change login between rotate and r-rotate

--- src/reverse_rotate.c
+++ src/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 18:11:17 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/25 18:22:06 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/27 20:25:53 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ bool	rra(t_push_swap *ps)
 
 	if (ps->top_a < 0)
 		return (false);
-	tmp = ps->stack_a[ps->top_a];
+	tmp = ps->stack_a[0];
 	i = -1;
 	while (++i < ps->top_a)
-		ps->stack_a[i + 1] = ps->stack_a[i];
-	ps->stack_a[0] = tmp;
+		ps->stack_a[i] = ps->stack_a[i + 1];
+	ps->stack_a[ps->top_a] = tmp;
 	return (true);
 }
 
@@ -34,11 +34,11 @@ bool	rrb(t_push_swap *ps)
 
 	if (ps->top_b < 0)
 		return (false);
-	tmp = ps->stack_b[ps->top_b];
+	tmp = ps->stack_b[0];
 	i = -1;
 	while (++i < ps->top_b)
-		ps->stack_b[i + 1] = ps->stack_b[i];
-	ps->stack_b[0] = tmp;
+		ps->stack_b[i] = ps->stack_b[i + 1];
+	ps->stack_b[ps->top_b] = tmp;
 	return (true);
 }
 
