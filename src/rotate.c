a7f206025d978504e68a4d7b069993398850c4a5
zhunhe <xuzhunhe@gmail.com>
2022-02-28 17:53:10 +0900

    fix: Fix ra, rb to avoid overlap

--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 17:46:44 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/28 17:12:23 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/28 17:52:22 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ bool	ra(t_push_swap *ps, bool prints_func)
 	if (ps->a.top < 0)
 		return (false);
 	tmp = ps->a.stack[ps->a.top];
-	i = -1;
-	while (++i < ps->a.top)
+	i = ps->a.top;
+	while (--i >= 0)
 		ps->a.stack[i + 1] = ps->a.stack[i];
 	ps->a.stack[0] = tmp;
 	if (prints_func)
@@ -37,8 +37,8 @@ bool	rb(t_push_swap *ps, bool prints_func)
 	if (ps->b.top < 0)
 		return (false);
 	tmp = ps->b.stack[ps->b.top];
-	i = -1;
-	while (++i < ps->b.top)
+	i = ps->b.top;
+	while (--i >= 0)
 		ps->b.stack[i + 1] = ps->b.stack[i];
 	ps->b.stack[0] = tmp;
 	if (prints_func)
