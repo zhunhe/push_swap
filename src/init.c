b267ea010fb6a3ac8ff4d2bd668769868e10080b
zhunhe <xuzhunhe@gmail.com>
2022-02-28 12:40:47 +0900

    feat: Set 0 to lis[]

--- src/init.c
+++ src/init.c
@@ -6,12 +6,21 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 12:11:41 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/28 12:17:37 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/28 12:39:56 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "push_swap.h"
+
+static void	set_zero(int *array, int count)
+{
+	int	i;
+
+	i = -1;
+	while (++i < count)
+		array[i] = 0;
+}
 
 void	init_push_swap(t_push_swap *ps, int count)
 {
@@ -35,6 +44,7 @@ void	init_push_swap(t_push_swap *ps, int count)
 		ps->lis[i] = (int *)malloc(sizeof(int) * ps->count);
 		if (ps->nums[i] == NULL || ps->tmp[i] == NULL || ps->lis[i] == NULL)
 			quit_push_swap(ps, MALLOC_ERROR);
+		set_zero(ps->lis[i], ps->count);
 	}
 	ps->a.top = ps->count - 1;
 	ps->b.top = -1;
