e0a4c8199511a81d8a75c242704b2c6c7ec04b18
zhunhe <xuzhunhe@gmail.com>
2022-02-28 20:33:30 +0900

    feat: Align both sides of stack a

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,32 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 12:53:45 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/28 18:39:54 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/28 20:29:13 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static bool	is_sorted(t_push_swap *ps)
+{
+	int	i;
+
+	i = -1;
+	while (++i < ps->count)
+		if (ps->a.stack[i] != ps->count - i)
+			return (false);
+	return (true);
+}
+
+static void	align(t_push_swap *ps)
+{
+	if (ps->a.stack[ps->a.top] == ps->count)
+		ra(&ps, true);
+	else if (ps->a.stack[1] == ps->count)
+		rra(&ps, true);
+	if (ps->a.stack[ps->a.top] == ps->a.stack[ps->a.top - 1] + 1)
+		sa(&ps, true);
+}
 
 static void	a_to_b(t_push_swap *ps)
 {
@@ -30,6 +51,8 @@ static void	a_to_b(t_push_swap *ps)
 			pb(ps);
 			--ps->lis.to_b_count;
 		}
+		if (is_sorted(ps))
+			break ;
 	}
 }
 
@@ -39,6 +62,10 @@ void	push_swap(int count, char **nums)
 
 	init_push_swap(&ps, count);
 	to_integer(&ps, nums);
-	lis(&ps);
-	a_to_b(&ps);
+	align(&ps);
+	if (!is_sorted(&ps))
+	{
+		lis(&ps);
+		a_to_b(&ps);
+	}
 }
