d24ff2e7596113096084a20bfb1ae1854c09a1ee
zhunhe <xuzhunhe@gmail.com>
2022-03-03 18:20:31 +0900

    refactor: Change for norminette

--- src/b_to_a.c
+++ src/b_to_a.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 10:41:09 by juhur             #+#    #+#             */
-/*   Updated: 2022/03/03 16:42:37 by juhur            ###   ########.fr       */
+/*   Updated: 2022/03/03 16:56:32 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,26 @@ static int	get_index_a(t_push_swap *ps, int val)
 	return (ps->a.top);
 }
 
+static void	set_data(int *array, int index, int value)
+{
+	int	i;
+
+	if (index == RB || index == RRB || index == RA || index == RRA)
+		array[index] = value;
+	else if (index == RR || index == RRR)
+	{
+		array[index] = value;
+		array[index + 1] -= array[index];
+		array[index + 2] -= array[index];
+	}
+	else if (index == COUNT)
+	{
+		i = RR - 1;
+		while (++i < RRB + 1)
+			array[COUNT] += array[i];
+	}
+}
+
 static void	set_move_data(t_push_swap *ps)
 {
 	int	i;
@@ -50,27 +70,21 @@ static void	set_move_data(t_push_swap *ps)
 	{
 		fill_n(ps->tmp[i], 7, 0);
 		if (i >= ps->b.top / 2)
-			ps->tmp[i][RB] = ps->b.top - i;
+			set_data(ps->tmp[i], RB, ps->b.top - i);
 		else
-			ps->tmp[i][RRB] = i + 1;
+			set_data(ps->tmp[i], RRB, i + 1);
 		j = get_index_a(ps, ps->b.stack[i]);
 		if (j >= ps->a.top / 2)
-			ps->tmp[i][RA] = ps->a.top - j;
+			set_data(ps->tmp[i], RA, ps->a.top - j);
 		else
-			ps->tmp[i][RRA] = j + 1;
-		ps->tmp[i][RR] = min(ps->tmp[i][RA], ps->tmp[i][RB]);
-		ps->tmp[i][RA] -= ps->tmp[i][RR];
-		ps->tmp[i][RB] -= ps->tmp[i][RR];
-		ps->tmp[i][RRR] = min(ps->tmp[i][RRA], ps->tmp[i][RRB]);
-		ps->tmp[i][RRA] -= ps->tmp[i][RRR];
-		ps->tmp[i][RRB] -= ps->tmp[i][RRR];
-		j = RR - 1;
-		while (++j < RRB + 1)
-			ps->tmp[i][COUNT] += ps->tmp[i][j];
+			set_data(ps->tmp[i], RRA, j + 1);
+		set_data(ps->tmp[i], RR, min(ps->tmp[i][RA], ps->tmp[i][RB]));
+		set_data(ps->tmp[i], RRR, min(ps->tmp[i][RRA], ps->tmp[i][RRB]));
+		set_data(ps->tmp[i], COUNT, 0);
 	}
 }
 
-static void	move_to_a(t_push_swap *ps)
+static int	*get_array(t_push_swap *ps)
 {
 	int	i;
 	int	idx;
@@ -86,26 +100,29 @@ static void	move_to_a(t_push_swap *ps)
 			idx = i;
 		}
 	}
-	if (ps->tmp[i][RR] > 0)
-		rr(ps, ps->tmp[i][RR]);
-	if (ps->tmp[i][RA] > 0)
-		ra(ps, ps->tmp[i][RA], true);
-	if (ps->tmp[i][RB] > 0)
-		rb(ps, ps->tmp[i][RB], true);
-	if (ps->tmp[i][RRR] > 0)
-		rrr(ps, ps->tmp[i][RRR]);
-	if (ps->tmp[i][RRA] > 0)
-		rra(ps, ps->tmp[i][RRA], true);
-	if (ps->tmp[i][RRB] > 0)
-		rrb(ps, ps->tmp[i][RRB], true);
-	pa(ps);
+	return (ps->tmp[idx]);
 }
 
 void	b_to_a(t_push_swap *ps)
 {
+	int	*array;
+
 	while (ps->b.top >= 0)
 	{
 		set_move_data(ps);
-		move_to_a(ps);
+		array = get_array(ps);
+		if (array[RR] > 0)
+			rr(ps, array[RR]);
+		if (array[RA] > 0)
+			ra(ps, array[RA], true);
+		if (array[RB] > 0)
+			rb(ps, array[RB], true);
+		if (array[RRR] > 0)
+			rrr(ps, array[RRR]);
+		if (array[RRA] > 0)
+			rra(ps, array[RRA], true);
+		if (array[RRB] > 0)
+			rrb(ps, array[RRB], true);
+		pa(ps);
 	}
 }
