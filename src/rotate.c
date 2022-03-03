50ce8832c2ea7252d0b5e154a5f12abed332da81
zhunhe <xuzhunhe@gmail.com>
2022-03-03 16:43:27 +0900

    refactor: Update ra, rb
    - Use deep_copy()

--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 17:46:44 by juhur             #+#    #+#             */
-/*   Updated: 2022/03/03 15:28:40 by juhur            ###   ########.fr       */
+/*   Updated: 2022/03/03 16:31:12 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,11 @@ bool	ra(t_push_swap *ps, int count, bool prints_func)
 	tmp = (int *)malloc(sizeof(int) * count);
 	if (tmp == NULL)
 		quit_push_swap(ps, ERROR);
-	i = -1;
-	while (++i < count)
-		tmp[i] = ps->a.stack[ps->a.top - count + 1 + i];
+	deep_copy(tmp, ps->a.stack + ps->a.top - count + 1, count);
 	i = ps->a.top - count + 1;
 	while (--i >= 0)
 		ps->a.stack[i + count] = ps->a.stack[i];
-	i = -1;
-	while (++i < count)
-		ps->a.stack[i] = tmp[i];
+	deep_copy(ps->a.stack, tmp, count);
 	free(tmp);
 	tmp = NULL;
 	if (prints_func)
@@ -50,15 +46,11 @@ bool	rb(t_push_swap *ps, int count, bool prints_func)
 	tmp = (int *)malloc(sizeof(int) * count);
 	if (tmp == NULL)
 		quit_push_swap(ps, ERROR);
-	i = -1;
-	while (++i < count)
-		tmp[i] = ps->b.stack[ps->b.top - count + 1 + i];
+	deep_copy(tmp, ps->b.stack + ps->b.top - count + 1, count);
 	i = ps->b.top - count + 1;
 	while (--i >= 0)
 		ps->b.stack[i + count] = ps->b.stack[i];
-	i = -1;
-	while (++i < count)
-		ps->b.stack[i] = tmp[i];
+	deep_copy(ps->b.stack, tmp, count);
 	free(tmp);
 	tmp = NULL;
 	if (prints_func)
